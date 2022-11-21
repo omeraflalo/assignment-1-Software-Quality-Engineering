4d914a1741ea55eeb5421725a3a52c32332c2b97
Ariel-Pich <arielpich3@gmail.com>
2022-11-21 18:20:12 +0200

    setUpForTesting

--- src/test/java/ShoppingListTest.java
+++ src/test/java/ShoppingListTest.java
@@ -1,26 +1,37 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import sise.sqe.Product;
 import sise.sqe.ShoppingList;
 import sise.sqe.Supermarket;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 public class ShoppingListTest {
-    @Test
-    public void testShoppingListConstructor() {
-        Supermarket supermarket = Mockito.mock(Supermarket.class);
-        assertNotNull(new ShoppingList(supermarket));
+    private Supermarket sm;
+    private ShoppingList shoppingList;
+    private ArrayList<Product> listOfProductsToAdd;
+
+    @BeforeAll
+    public void setUpForTesting(){
+        sm = mock(Supermarket.class);
+        shoppingList = new ShoppingList(sm);
+        for (int i =0;i<16;i++){
+            listOfProductsToAdd.add(new Product(Integer.toString(i), "p" + Integer.toString(i), i));
+        }
+
     }
 
     @Test
     public void testAddProduct() {
-        ShoppingList shoppingList = new ShoppingList(mock(Supermarket.class));
         Product p = new Product("a","a",2);
         shoppingList = spy(shoppingList);
         shoppingList.addProduct(p);
         verify(shoppingList).addProduct(p);
+        assert shoppingList.getMarketPrice() == sm.getPrice(p.getId()); //a creative way to make sure the product added
     }
 
     @Test
