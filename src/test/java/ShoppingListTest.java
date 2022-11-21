0b9d7549e83c625c3170800a4c2829ebea54a764
Omer Aflalo <omer.aflalo@gmail.com>
2022-11-21 18:29:46 +0200

    add products to shoppingList BeforeAll Func

--- src/test/java/ShoppingListTest.java
+++ src/test/java/ShoppingListTest.java
@@ -11,40 +11,41 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 public class ShoppingListTest {
-    private Supermarket sm;
+    private Supermarket supermarket;
     private ShoppingList shoppingList;
     private ArrayList<Product> listOfProductsToAdd;
 
+
+    public void addProductWithMock(Product product, double price) {
+        shoppingList.addProduct(product);
+        when(supermarket.getPrice(product.productId)).thenReturn(price);
+    }
+
     @BeforeAll
-    public void setUpForTesting(){
-        sm = mock(Supermarket.class);
-        shoppingList = new ShoppingList(sm);
-        for (int i =0;i<16;i++){
-            listOfProductsToAdd.add(new Product(Integer.toString(i), "p" + Integer.toString(i), (int)(Math.random()*100)+1));
+    public void setUpForTesting() {
+        supermarket = mock(Supermarket.class);
+        shoppingList = new ShoppingList(supermarket);
+        for (int i = 0; i < 16; i++) {
+            listOfProductsToAdd.add(new Product(Integer.toString(i), "p" + Integer.toString(i), (int) (Math.random() * 100) + 1));
+        }
+        for (Product p : listOfProductsToAdd) {
+            addProductWithMock(p, (int)(Math.random()*1000)+1);
         }
 
     }
 
     @Test
     public void testAddProduct() {
-        Product p = new Product("a","a",2);
+        Product p = new Product("a", "a", 2);
         shoppingList = spy(shoppingList);
         shoppingList.addProduct(p);
         verify(shoppingList).addProduct(p);
-        assert shoppingList.getMarketPrice() == sm.getPrice(p.getId()); //a creative way to make sure the product added
+        assert shoppingList.getMarketPrice() == supermarket.getPrice(p.getId()); //a creative way to make sure the product added
     }
 
-    public void addProductWithMock(Supermarket supermarket,ShoppingList shoppingList, Product product, double price){
-        shoppingList.addProduct(product);
-        when(supermarket.getPrice(product.productId)).thenReturn(price);
-    }
 
     @Test
     public void testGetMarketPrice() {
-        Supermarket supermarket = mock(Supermarket.class);
-        ShoppingList shoppingList = new ShoppingList(supermarket);
-        Product p = new Product("a","a",2);
-        addProductWithMock(supermarket,shoppingList,p,8);
 
     }
 
